a9a7285b81998ceb0c946c03716caaf4ef87e04c
Munh-Orgil123 <160573798+Munh-Orgil123@users.noreply.github.com>
2024-02-20 14:20:31 +0800

    Update haha.java

--- lab1/src/main/java/lab1/haha.java
+++ lab1/src/main/java/lab1/haha.java
@@ -4,7 +4,7 @@ package lab1;
 public class haha {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub bfouwehfowhifhwohefowehfoh
 			System.out.println("hihi");
 	}
 
